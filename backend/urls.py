6cdeec98ad21d10000b5518746e8fbad6ae715e6
NOir2217 <000noir00@gmail.com>
2026-07-19 10:29:00 +0545

    Import re_path and serve for static file handling

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
