9da78055a93c1f8ceb4953ff0a1ac55384f4b4bf
jkldfssdfjkl <248022_aayush@kusom.edu.np>
2026-07-19 10:06:22 +0545

    some more changes

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -32,3 +32,23 @@ JWT_COOKIE_SAMESITE = 'Strict' # Prevent CSRF
 # Optional: Set a strict CSP via django-csp or manual middleware here if needed
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
